b1b38bd94be0314eef1069084aebb5fd00a40277
Adam <adambasilio211@gmail.com>
2022-09-06 15:40:34 -0300

    new plot functions

--- graphs.py
+++ graphs.py
@@ -4,28 +4,68 @@ import seaborn as sns
     
 plt.rcParams['font.size'] = 12
 
-def zcount(filename):
+def __get_data(filename):
     data = pd.read_csv(f'resultados/{filename}.csv')
+    z_pos = data.z.unique()
+    x_pos = data.x.unique()
+
     plot_data = {
-        'z': data.z.unique(),
+        'z': [],
+        'x': [],
         'count': []
     }
 
-    for item_z in plot_data['z']:
-        val = data.query('z == @item_z').contagem.mean()
-        plot_data['count'].append(val)
+    for item_x in x_pos:
+        for item_z in z_pos:
+            val = data.query('z == @item_z and x == @item_x').contagem.mean()
+            plot_data['count'].append(val)
+            plot_data['x'].append(item_x)
+            plot_data['z'].append(item_z)
 
     plot_data = pd.DataFrame(plot_data)
+    return plot_data
+
+def zcount(filename):
+    plot_data = __get_data(filename)
 
     sns.scatterplot(
         data = plot_data,
         x = 'z',
-        y = 'count'
+        y = 'count',
+        hue = 'x',
+        style = 'x'
     )
     plt.xticks(plot_data['z'])
     plt.xlabel('z (cm)')
-    # plt.legend(title = 'x (cm)', bbox_to_anchor=(1,1))
+    plt.legend(title = 'x (cm)', bbox_to_anchor=(1,1))
     plt.savefig('imgs/z-contagem.png')
-#     plt.show()
+    plt.show()
 
-# zcount('teste_v2')
+def xcount(filename):
+    plot_data = __get_data(filename)
+
+    sns.scatterplot(
+        data = plot_data,
+        x = 'x',
+        y = 'count',
+        hue = 'z',
+        style = 'z'
+    )
+    plt.xticks(plot_data['x'])
+    plt.xlabel('x (cm)')
+    plt.legend(title = 'z (cm)', bbox_to_anchor=(1,1))
+    plt.savefig('imgs/x-contagem.png')
+    plt.show()
+
+def count_over_time(filename, x_pos, z_pos):
+    data = pd.read_csv(f'resultados/{filename}.csv')
+    plot_data = data.query('z == @z_pos and x == @x_pos')
+
+    sns.scatterplot(
+        data = plot_data,
+        x = 'tempo(s)',
+        y = 'contagem'
+    )
+    plt.savefig('imgs/count_over_time.png')
+    plt.title(f'x = {x_pos} cm ---- z = {z_pos} cm')
+    plt.show()
